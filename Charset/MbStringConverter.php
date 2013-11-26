354b082ba9a70d06ec594ca2f5a969f1a1dea6ff
Antonio J. García Lagar <aj@garcialagar.es>
2013-11-26 18:52:47 +0100

    Avoid dynamic config modification

--- Charset/MbStringConverter.php
+++ Charset/MbStringConverter.php
@@ -20,14 +20,8 @@ class MbStringConverter
      */
     public function convert($value, $inputCharset, $outputCharset)
     {
-        /**
-         * @see http://www.php.net/manual/en/function.iconv.php#108643
-         */
         if ($inputCharset != $outputCharset) {
-            $char = ini_get('mbstring.substitute_character');
-            ini_set('mbstring.substitute_character', 32);
             $value = mb_convert_encoding($value, $outputCharset, $inputCharset);
-            ini_set('mbstring.substitute_character', $char);
         }
 
         return $value;
