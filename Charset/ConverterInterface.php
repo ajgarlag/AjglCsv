b9f749260ca6ce0e79f5e7d26a5580cca2a9c233
Antonio J. García Lagar <aj@garcialagar.es>
2014-03-18 18:40:44 +0100

    Updated copyright year

--- Charset/ConverterInterface.php
+++ Charset/ConverterInterface.php
@@ -2,7 +2,7 @@
 /**
  * This file is part of the AJ General Libraries
  *
- * Copyright (C) 2010-2013 Antonio J. García Lagar <aj@garcialagar.es>
+ * Copyright (C) 2010-2014 Antonio J. García Lagar <aj@garcialagar.es>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
